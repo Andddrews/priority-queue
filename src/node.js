08ec198b8aa62cc45fb96f53c85688f0c9eb4c8a
Andrei Savastsijonak <Andddrews@gmail.com>
2019-03-16 19:59:15 +0300

    18

--- src/node.js
+++ src/node.js
@@ -1,22 +1,65 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null
 	}
 
 	appendChild(node) {
-
+		if (!this.left){
+			this.left = node;
+			node.parent = this;
+		}else if (!this.right){
+			this.right =node;
+			node.parent = this;
+		}else{
+			return;
+			}
 	}
 
 	removeChild(node) {
+		if (this.left == node) {
+			this.left = null;
+			node.parent = null;
+		} else if (this.right == node) {
+			this.right = null;
+			node.parent = null;
+		} else {
+			throw 'blablabla';
+		}
 
 	}
 
 	remove() {
-
+		if (this.parent == null) {
+			return;
+		} else {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+			if (this.parent == null) {
+			return;
+		} else {
+			const parent = this.parent
+			const parentOfParent = parent.parent;
+			this.parent = parentOfParent;
+			parent.parent = this;
+
+			if (parent.left && parent.left !== this) {
+				parent.left.parent = this;
+			}
+
+			if (parent.right && parent.right !== this) {
+				parent.right.parent = this;
+			}
+
+			
+		}
+	
 	}
 }
 
