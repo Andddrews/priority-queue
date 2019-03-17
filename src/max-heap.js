955857b2860aa0221c592fb9dd62839f451f6b4c
Andrei Savastsijonak <Andddrews@gmail.com>
2019-03-17 18:22:11 +0300

    23 passed

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,14 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes=[];
 	}
 
 	push(data, priority) {
-		
+		if (this.data && this.priority){
+			[].push(this.data, this.priority);
+		}
 	}
 
 	pop() {
