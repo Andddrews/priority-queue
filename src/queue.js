f483ff22199fc448600ace48d6499f73fe9d5948
Andrei Savastsijonak <Andddrews@gmail.com>
2019-03-17 18:53:33 +0300

    23 vs 31

--- src/queue.js
+++ src/queue.js
@@ -2,7 +2,12 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize;
+if (this != 30 ){
+q=this;
+}else{
+	qWithDefaultMaxSize=this;
+}
 	}
 
 	push(data, priority) {
